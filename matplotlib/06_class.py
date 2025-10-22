cf225563c444a20107c9b5c39e65b11728a52fb0
SHREYAN <shreyandey1234@gmail.com>
2025-10-22 12:44:39 +0530

    modify 06_class

--- matplotlib/06_class.py
+++ matplotlib/06_class.py
@@ -7,8 +7,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = [0, 1, 2, 3, 4, 5, 6, 7, 7, 8, 10]
-y = [55, 60, 65, 62, 68, 70, 75, 78, 82, 85, 87]
+x1 = np.array([0, 1, 2, 3, 4, 5, 6, 7, 7, 8, 10])
+y1 = np.array([55, 60, 65, 62, 68, 70, 75, 78, 82, 85, 87])
 
-plt.scatter(x, y)
+x2 = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 10])
+y2 = np.array([50, 58, 65, 70, 72, 78, 83, 88, 92, 95, 97])
+
+plt.scatter(x1, y1, s = 20,
+                    label = "Class A")
+plt.scatter(x2, y2, s = 20,
+                    label = "Class B")
+
+plt.title("Test Score")
+plt.xlabel("Hours Studied")
+plt.ylabel("Grade")
+
+plt.legend()
 plt.show()
